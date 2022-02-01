5cccb872f68b1d69349bd28676a9ff67d4c23552
Star Brilliant <coder@poorlab.com>
2022-02-01 05:30:21 +0000

    Fix negative values for PQ

--- gammas.py
+++ gammas.py
@@ -137,7 +137,8 @@ def PQ_EOTF(buf: np.ndarray) -> np.ndarray:
     c1 = 3424 / 4096
     c2 = 2413 / 4096 * 32
     c3 = 2392 / 4096 * 32
-    return 10000 * (np.maximum(buf**(1 / m2) - c1, 0) / (c2 - c3 * buf**(1 / m2)))**(1 / m1)
+    buf_abs = np.abs(buf)
+    return 10000 * (np.maximum(buf_abs**(1 / m2) - c1, 0) / (c2 - c3 * buf_abs**(1 / m2)))**(1 / m1) * np.sign(buf)
 
 
 # https://www.w3.org/TR/css-color-4/#color-conversion-code
